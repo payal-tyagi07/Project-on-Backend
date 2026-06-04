8d4eccbf822431b58da3f70d3753187eef48fa6f
001 <payaltyagi727@gmail.com>
2026-06-05 02:11:20 +0530

    updated app.js studied utils,cookieParsor,etc

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const app=express();
 
 dotenv.config();
 
-
 connectDB()
-
-
-
-
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed");
+})
 
 
 
