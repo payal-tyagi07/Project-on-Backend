b97b38f7955977a54b8f3dd497223ddaa06d1346
001 <payaltyagi727@gmail.com>
2026-06-25 15:56:27 +0530

    feat: reorganize user routes and add new endpoints for account management and user profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,13 @@
 import {Router} from "express";
-import {refreshAccessToken, registerUser} from "../controllers/user.controller.js";
+import {changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory, 
+    refreshAccessToken, 
+    registerUser, updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage}
+    from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {loginUser} from "../controllers/user.controller.js";
@@ -28,8 +36,22 @@ router.route("/login").post(loginUser)
 
 //secured Routes
 router.route("/logout").post(verifyJWT,logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+
+//verifyJWT middleware passed because we want to execute this after login
+router.route("/changePassword").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").post(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+//2 middlewares are used here
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT,upload.single("cover-image"),updateUserCoverImage)
+
+//data from params
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT,getWatchHistory)
+
 
 
 export default router; 
