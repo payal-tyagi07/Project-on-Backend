3e7bbcdefbeeeb2ce1144fcd1c8ce22bc049d66b
001 <payaltyagi727@gmail.com>
2026-07-02 23:22:53 +0530

    refactor: clean up asyncHandler code by commenting out unused promise-based implementation

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,33 +1,32 @@
-//METHOD-1
+// //METHOD-1
 
-//promises code
-const asyncHandler=(requestHandler)=>{
-    return (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).
-        catch((err)=>next(err));
-    }
-}
+// //promises code
+// const asyncHandler=(requestHandler)=>{
+//     return (req,res,next)=>{
+//         Promise.resolve(requestHandler(req,res,next)).
+//         catch((err)=>next(err));
+//     }
+// }
 
-
-
-export default asyncHandler;
 
 //METHOD-2
 
 // try catch code
 
-// const asyncHandler=(fn)=>{
-// return async (req,res,next) => {
-//     try{
-//         await fn(req,res,next);
-//     }
-//     catch(err){
-//         res.status(err.code || 500).json({
-//             success:false,
-//             message:err.message || "Internal Server Error"
-//         })
-//     }
-// }}
+const asyncHandler=(fn)=>{
+return async (req,res,next) => {
+    try{
+        await fn(req,res,next);
+    }
+    catch(err){
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message || "Internal Server Error"
+        })
+    }
+}}
+
+export default asyncHandler;
 
 
 
